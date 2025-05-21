60a93f71dbb436d459cfd9ee866fb2868df82276
Sujaan <sujaan677@gmail.com>
2025-05-21 18:09:21 +0530

    Update drafting_agent.py

--- agents/drafting_agent.py
+++ agents/drafting_agent.py
@@ -1,4 +1,4 @@
-from langchain_groq import ChatGroq  # ✅ correct
+from langchain_groq import ChatGroq  
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableSequence
 
